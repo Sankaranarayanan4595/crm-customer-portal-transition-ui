9c738f402b8a81149e1e99bcc80996bc61c9e6e0
Sankaranarayanan <sankaranarayanan.saminathan@adamsbridge.tech>
2026-06-01 20:10:41 +0530

    unused components removed

--- src/app/components/sharedUI/pipes/remove-hyphen-spaces.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { RemoveHyphenSpacesPipe } from './remove-hyphen-spaces.pipe';
-
-describe('RemoveHyphenSpacesPipe', () => {
-  it('create an instance', () => {
-    const pipe = new RemoveHyphenSpacesPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/app/components/sharedUI/pipes/truncate-html.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TruncateHtmlPipe } from './truncate-html.pipe';
-
-describe('TruncateHtmlPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TruncateHtmlPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
